30d30faff223cb493bc2cd83670cd633590821e3
Ankil Naik <anaik5@ford.com>
2021-03-22 22:58:45 -0400

    additions to header

--- src/components/header.js
+++ src/components/header.js
@@ -5,6 +5,10 @@ const Header = () => {
     return (
         <div className="header">
             <h1 className="pane">HOME</h1>
+            <h1 className="pane">ABOUT ME</h1>
+            <h1 className="pane">EXPERIENCE</h1>
+            <h1 className="pane">EDUCATION</h1>
+            <h1 className="pane">CONTACT</h1>
         </div>
     );
 }
