2342e978dc63d4fe0856878ac91a0ce6a0a3882b
anaik5 <anaik5@ford.com>
2022-05-07 01:04:13 -0400

    added text and images

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from '../components/header.js';
 import '../styles/aboutMe.css';
+import { aboutMeText } from '../content/aboutMe.json';
+import img1 from '../images/Reco-1.png';
 
 const aboutMe = () => {
-  const message = "Please come back at a later point...I am building something exciting";
   ReactDOM.render(
     <div>
       <Header />
-      <p className="aboutMeMsg">{message}</p>
+      <p className="aboutMeMsg">{aboutMeText}</p>
+      <img src={img1} className="img"></img>
     </div>,
     document.getElementById('root')
   );
--- src/components/header.js
+++ src/components/header.js
@@ -6,8 +6,8 @@ import Home from './home';
 const Header = () => {
     return (
         <div className="header">
-            <a href="#" className="href" onClick={Home}><h1 className="pane">HOME</h1></a>
-            <a href="#" className="href" onClick={aboutMe}><h1 className="pane">ABOUT ME</h1></a>
+            <a href="#/home" className="href" onClick={Home}><h1 className="pane">HOME</h1></a>
+            <a href="#/about-me" className="href" onClick={aboutMe}><h1 className="pane">ABOUT ME</h1></a>
             <a href="#" className="href"><h1 className="pane">EXPERIENCE</h1></a>
             <a href="#" className="href"><h1 className="pane">EDUCATION</h1></a>
             <a href="#" className="href"><h1 className="pane">CONTACT</h1></a>
