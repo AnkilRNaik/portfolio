0490c0f283347eed2bb25d92f932b0075e4a480a
anaik5 <anaik5@ford.com>
2022-05-07 15:06:39 -0400

    getting the about me page ready

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -3,17 +3,18 @@ import ReactDOM from 'react-dom';
 import Header from '../components/header.js';
 import '../styles/aboutMe.css';
 import { aboutMeText } from '../content/aboutMe.json';
-import img1 from '../images/Reco-1.png';
+import img from '../images/recommendations.png';
 
-const aboutMe = () => {
+const AboutMe = () => {
   ReactDOM.render(
     <div>
       <Header />
       <p className="aboutMeMsg">{aboutMeText}</p>
-      <img src={img1} className="img"></img>
+      <img src={img} className="borderImg" />
+      <a href="" target="_blank" className="buttonH"><h3 className="h">Click to see my Resume</h3></a>
     </div>,
     document.getElementById('root')
   );
 }
 
-export default aboutMe;
+export default AboutMe;
--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import '../styles/header.css';
-import aboutMe from './aboutMe';
+import AboutMe from './aboutMe';
 import Home from './home';
 
 const Header = () => {
     return (
         <div className="header">
             <a href="#/home" className="href" onClick={Home}><h1 className="pane">HOME</h1></a>
-            <a href="#/about-me" className="href" onClick={aboutMe}><h1 className="pane">ABOUT ME</h1></a>
+            <a href="#/about-me" className="href" onClick={AboutMe}><h1 className="pane">ABOUT ME</h1></a>
             <a href="#" className="href"><h1 className="pane">EXPERIENCE</h1></a>
             <a href="#" className="href"><h1 className="pane">EDUCATION</h1></a>
             <a href="#" className="href"><h1 className="pane">CONTACT</h1></a>
