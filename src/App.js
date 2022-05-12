a52443cc074e125c2a7867ad98f466406e7423fe
anaik5 <anaik5@ford.com>
2022-05-12 12:08:40 -0400

    reverting back the changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from '../src/components/home.js';
 import AboutMe from './components/aboutMe.js';
 import Experience from './components/experience.js';
@@ -9,16 +9,16 @@ import Contact from './components/contact.js';
 
 const App = () => {
   return (
-    <div>
+    <Router basename="/portfolio">
       <Header />
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route exact path="*" element={<Home />} />
         <Route path="/about-me" element={<AboutMe />} />
         <Route path="/experience" element={<Experience />} />
         <Route path="/education" element={<Education />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </div>
+    </Router>
   );
 }
 
--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ import '../styles/header.css';
 function Header() {
     return (
         <div className="header">
-            <Link exact to="/" className="href"><h1 className="pane">HOME</h1></Link>
+            <Link exact to="/header" className="href"><h1 className="pane">HOME</h1></Link>
             <Link to="/about-me" className="href"><h1 className="pane">ABOUT ME</h1></Link>
             <Link to="/experience" className="href"><h1 className="pane">EXPERIENCE</h1></Link>
             <Link to="/education" className="href"><h1 className="pane">EDUCATION</h1></Link>
